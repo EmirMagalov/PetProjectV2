21aa0c824b0617b453d2e5fd0e0fc41360a7a876
Emir Magalov <emir.mahalo@gmail.com>
2026-09-20 16:14:14 +0400

    update start menu

--- src/bot/handlers/user_handler.py
+++ src/bot/handlers/user_handler.py
@@ -3,9 +3,14 @@ from aiogram.filters import CommandStart, Command
 from common.config import settings
 from backend.models.pet import Pet as PetModel
 from tortoise.expressions import F as db_f
+from aiogram.types import FSInputFile
+from aiogram.utils.media_group import MediaGroupBuilder
 user_router = Router()
 
-
+PHOTO_CACHE = {
+    "photo1": None,
+    "photo2": None
+}
 @user_router.message(Command("start"))
 async def start_handler(message: types.Message):
     keyboard = types.InlineKeyboardMarkup(
@@ -18,13 +23,30 @@ async def start_handler(message: types.Message):
             ]
         ]
     )
-    text = (
-        "Привет! 👋 Добро пожаловать в нашу уютную игру!\n\n"
-        "Здесь ты сможешь заботиться о своем питомце, кормить его вкусняшками из холодильника, купать и играть.🎮\n\n"
-        "Нажимай кнопку ниже, чтобы запустить приложение и начать приключение! 🐾"
-    )
-    await message.answer(text, reply_markup=keyboard)
 
+    media_group = MediaGroupBuilder(
+        caption="Привет! 👋 Добро пожаловать в нашу уютную игру!\n\nЗдесь ты сможешь заботиться о своем питомце... 🐾"
+    )
+
+    # Если file_id еще не сохранен — загружаем с диска, иначе берем из кэша
+    p1 = PHOTO_CACHE["photo1"] or FSInputFile("bot/photo/instructions1.jpg")
+    p2 = PHOTO_CACHE["photo2"] or FSInputFile("bot/photo/instructions2.jpg")
+
+    media_group.add_photo(media=p1)
+    media_group.add_photo(media=p2)
+
+    # Отправляем альбом и забираем ответ от Telegram, чтобы сохранить file_id
+    sent_messages = await message.answer_media_group(media=media_group.build())
+
+    # Если мы отправляли файлы с диска, Telegram пришлет нам объекты с уже готовыми file_id
+    if not PHOTO_CACHE["photo1"] and sent_messages:
+        PHOTO_CACHE["photo1"] = sent_messages[0].photo[-1].file_id
+        PHOTO_CACHE["photo2"] = sent_messages[1].photo[-1].file_id
+
+    await message.answer(
+        "Нажимай кнопку ниже, чтобы запустить приложение и начать приключение! 👇",
+        reply_markup=keyboard
+    )
 
 
 
